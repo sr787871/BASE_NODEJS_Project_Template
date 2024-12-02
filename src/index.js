70fe497d0f6334cee69599b3c8d5b636ab5a44a9
Sachin Rawat <sr787871@gmail.com>
2024-12-02 12:09:16 +0530

    Basic Node Js Template

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const {serverConfig,logger} = require("./config");
+const express = require("express");
+const apiRoutes = require("./routes");
+
+const app = express();
+
+// app.get('api/v1/blogs',(req,res)=>{
+
+// });
+
+app.use('/api',apiRoutes);
+
+app.listen(serverConfig.PORT,()=>{
+    console.log(`Successfully started the server at PORT : ${serverConfig.PORT}`);
+    logger.info("Successfully started the server",{});
+})
